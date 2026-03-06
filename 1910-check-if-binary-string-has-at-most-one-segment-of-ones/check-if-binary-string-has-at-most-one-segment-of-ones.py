4af73f0283b8266c1f990af5f1b3abd6d73a935a
Korubilli venkata sai <korubillivenkatasai@gmail.com>
2026-03-06 21:35:59 +0530

    Time: 3 ms (4.02%) | Memory: 19.3 MB (77.09%) - LeetSync

---
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkOnesSegment(self, s: str) -> bool:
+        n=len(s)
+        for i in range(0,n-1):
+            if s[i] == '0' and s[i+1] == '1':
+                return False
+        return True
+
+        
