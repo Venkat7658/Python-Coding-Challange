f62ffb9fbfc0dac1592e6b606702a46a8da953aa
Korubilli venkata sai <korubillivenkatasai@gmail.com>
2026-03-07 13:09:08 +0530

    Time: 11 ms (39.86%) | Memory: 19.2 MB (87.80%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
@@ -1,8 +1,25 @@
 class Solution:
     def minOperations(self, s: str) -> int:
-        c, j, n = 0, 0, len(s)
-        for ch in s:
-            if int(ch) == j:
-                c += 1
-            j ^= 1
-        return min(c, n - c)
+        change_to_0101 = 0
+        change_to_1010 = 0
+
+        for i in range(len(s)):
+
+            # pattern: 0101...
+            if i % 2 == 0:   # even index
+                if s[i] == '0':
+                    change_to_0101 += 1
+            else:            # odd index
+                if s[i] == '1':
+                    change_to_0101 += 1
+
+
+            # pattern: 1010...
+            if i % 2 == 0:   # even index
+                if s[i] == '1':
+                    change_to_1010 += 1
+            else:            # odd index
+                if s[i] == '0':
+                    change_to_1010 += 1
+
+        return min(change_to_0101, change_to_1010)
